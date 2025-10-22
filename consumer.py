91ecd0446d323b467bc3290f16eeb211dc4423f0
carebears789 <kjarjava214@gmail.com>
2025-10-22 11:42:06 -0600

    Made it work

--- consumer.py
+++ consumer.py
@@ -10,14 +10,14 @@ def get_request(s3, bucket_name):
     
     keys = sorted(obj["Key"] for obj in response["Contents"])
     next_key = keys[0]
-    obj = s3_client.get_object(Bucket=bucket_name, Key=next_key)
+    obj = s3.get_object(Bucket=bucket_name, Key=next_key)
     data = json.loads(obj["Body"].read())
 
     s3.delete_object(Bucket=bucket_name, Key=next_key)
     return data
 
 def store_in_s3(s3, bucket_name, widget):
-    key - f"{widget['id']}.json"
+    key = f"{widget}.json"
     s3.put_object(Bucket=bucket_name, Key=key, Body=json.dumps(widget))
 
 def dynamo(dynamo, widget):
@@ -25,9 +25,9 @@ def dynamo(dynamo, widget):
 
 def process_request(request, storage, s3, dyanmo, target):
 
-
-    action = request.get("action")
-    widget = request.get("widget")
+    print(f"Processing request: {request}")
+    action = request.get("type")
+    widget = request.get("widgetId")
 
     if action == "create" or action == "update":
         if storage == "s3":
@@ -43,8 +43,9 @@ def main():
     parser = argparse.ArgumentParser(description="Widget Consumer")
     parser.add_argument("--storage", choices=["s3", "dynamo"], required=True,
                         help="Storage backend for widgets")
-    parser.add_argument("--interval", type=int, default=5, help="Polling interval in seconds")
+    parser.add_argument("--interval", type=int, default=.001, help="Polling interval in seconds")
     parser.add_argument("--table", help="DynamoDB table name (if using DynamoDB storage)")
+    parser.add_argument("--bucket", help="Bucket table name (if using S3 storage)")
     args = parser.parse_args()
     
 
@@ -52,11 +53,11 @@ def main():
     s3 = boto3.client("s3")
     dynamo = boto3.resource("dynamodb").Table(args.table) if args.table else None
     while True:
-        request = get_request(s3, "bucket2")
+        request = get_request(s3, "usu-cs5270-scuba-requests")
         if request:
-            process_request(request, args.storage, s3, dynamo, args.target)
+            process_request(request, args.storage, s3, dynamo, "usu-cs5270-scuba-web")
         else:
-            print("No requests found, sleeping...")
+            #print("No requests found, sleeping...")
         time.sleep(args.interval)
 
 if __name__ == "__main__":
