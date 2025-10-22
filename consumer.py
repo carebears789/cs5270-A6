3fdbe57abac47297678be38c08bee41ea6e614fd
carebears789 <kjarjava214@gmail.com>
2025-10-22 11:20:44 -0600

    made main

---
+++ consumer.py
@@ -0,0 +1,63 @@
+import argparse
+import boto3
+import json
+import time
+
+def get_request(s3, bucket_name):
+    response = s3.list_objects_v2(Bucket=bucket_name)
+    if "Contents" not in response:
+        return None
+    
+    keys = sorted(obj["Key"] for obj in response["Contents"])
+    next_key = keys[0]
+    obj = s3_client.get_object(Bucket=bucket_name, Key=next_key)
+    data = json.loads(obj["Body"].read())
+
+    s3.delete_object(Bucket=bucket_name, Key=next_key)
+    return data
+
+def store_in_s3(s3, bucket_name, widget):
+    key - f"{widget['id']}.json"
+    s3.put_object(Bucket=bucket_name, Key=key, Body=json.dumps(widget))
+
+def dynamo(dynamo, widget):
+    dynamo.put_item(Item=widget)
+
+def process_request(request, storage, s3, dyanmo, target):
+
+
+    action = request.get("action")
+    widget = request.get("widget")
+
+    if action == "create" or action == "update":
+        if storage == "s3":
+            store_in_s3(s3, target, widget)
+        else:
+            store_widget_dynamo(dynamo, widget)
+    elif action == "delete":
+        print("Delet not implemented yet")
+    else:
+        print(f"Unknown action: {action}")
+
+def main():
+    parser = argparse.ArgumentParser(description="Widget Consumer")
+    parser.add_argument("--storage", choices=["s3", "dynamo"], required=True,
+                        help="Storage backend for widgets")
+    parser.add_argument("--interval", type=int, default=5, help="Polling interval in seconds")
+    parser.add_argument("--table", help="DynamoDB table name (if using DynamoDB storage)")
+    args = parser.parse_args()
+    
+
+
+    s3 = boto3.client("s3")
+    dynamo = boto3.resource("dynamodb").Table(args.table) if args.table else None
+    while True:
+        request = get_request(s3, "bucket2")
+        if request:
+            process_request(request, args.storage, s3, dynamo, args.target)
+        else:
+            print("No requests found, sleeping...")
+        time.sleep(args.interval)
+
+if __name__ == "__main__":
+    main()
